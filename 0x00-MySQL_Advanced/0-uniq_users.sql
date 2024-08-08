8ab485fff340d676d303a7830bd5d8436037216a
yahia008 <yahyatijjani99@gmail.com>
2024-08-08 13:10:41 +0000

    user

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- Generates a table featuring distinct users.
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
